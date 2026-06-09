86ee0cf4d00cf2edf0881dd5795ab3fc2a3e2284
Ranjankumarjena-rj <ranjankumarjena090@gmail.com>
2026-06-09 10:42:17 +0530

    Update BackendFormApplicationTests.java

--- src/test/java/com/backendstsform/demo/BackendFormApplicationTests.java
+++ src/test/java/com/backendstsform/demo/BackendFormApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ALL)
 class BackendFormApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

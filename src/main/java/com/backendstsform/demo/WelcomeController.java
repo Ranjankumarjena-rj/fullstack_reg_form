0ad664caece02d4a54074115ed6ec64fed643d54
Ranjankumarjena-rj <ranjankumarjena090@gmail.com>
2026-06-09 17:45:33 +0530

    Update WelcomeController.java

--- src/main/java/com/backendstsform/demo/WelcomeController.java
+++ src/main/java/com/backendstsform/demo/WelcomeController.java
@@ -3,11 +3,10 @@ package com.backendstsform.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
-import com.backendstsform.demo.UserDto;
+import com.backendstsform.demo.userdto.UserDto;
 
 @Controller
 public class WelcomeController {
-
     @GetMapping("/")
     public String showForm(Model model) {
         model.addAttribute("userDto", new UserDto());

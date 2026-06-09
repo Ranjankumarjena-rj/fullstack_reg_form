972d670055949baae0ed60089a8c3560ac5643d6
Ranjankumarjena-rj <ranjankumarjena090@gmail.com>
2026-06-09 10:43:04 +0530

    Update BackendFormApplicationTests.java

--- src/test/java/com/backendstsform/demo/BackendFormApplicationTests.java
+++ src/test/java/com/backendstsform/demo/BackendFormApplicationTests.java
@@ -2,6 +2,7 @@ package com.backendstsform.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ALL)
